e0aa8dd220101c82305698d9a50ccc0523b9c57b
MAMBA-python <info@mamba-python.nl>
2023-01-27 09:17:01 +0100

    Retry plots graphs

--- streamlit-example.py
+++ streamlit-example.py
@@ -13,36 +13,29 @@ import matplotlib.pyplot as plt
 
 # %%
 
-st.markdown("""
-            # Example Streamlit
-            ## Because we can. 
-            
-            This works exactly the same as your markdown cells in 
-            jupyter notebook! You can write any pretty text here.
-            
-            1. `different` ways of highlighting text
-            2. like **bold** and *italic*
-            """)
-
 ### DATAFRAMES PLOTTEN
 # Test data
 df= pd.DataFrame(
     np.random.randn(10, 2),
     columns=['x', 'y'])
 
-st.markdown(
-    """
-    # Show your dataframe
-    """
-    )
+col1, col2 = st.columns(2) 
 
-st.markdown("## Area Chart")
-st.area_chart(df)
+def display_graph():
+    with col2:
+        if option == 'bar chart':
+            st.bar_chart(df)
+        elif option == 'area chart':
+            st.area_chart(df)
+        elif option == 'no chart':
+            st.write('no chart!')
 
-st.markdown("## Any matplotlib Figure")
-fig, ax = plt.subplots(1)
-df.plot(ax=ax)
-st.plotly_chart(fig)
-
-st.markdown("## Streamlit Bar Chart")
-st.bar_chart(df)
+with col1:
+    st.markdown("""
+                # Different charts
+                ## Choose your favourite chart
+                """)
+    
+    
+    option = st.selectbox("choose your chart", ["bar chart", "area chart", "no chart"])
+    submit_button = st.button(label='Submit', on_click=display_graph)
